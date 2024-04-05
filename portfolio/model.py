1b7d40ff5defdbc50a1c44d8f355c6b87ed3cc27
Swarnabha Halder <swarnabhahalder80137@gmail.com>
2024-04-06 00:10:35 +0530

    Portfolio Django

---
+++ portfolio/model.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+class Project(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    image = models.ImageField(upload_to='project_images/')
+    # Add more fields as needed
+    
+    def __str__(self):
+        return self.title
---
+++ portfolio/views.py
@@ -0,0 +1,14 @@
+# portfolio/views.py
+from django.shortcuts import render
+
+def index(request):
+    return render(request, 'index.html')
+
+def projects(request):
+    return render(request, 'projects.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+def contact(request):
+    return render(request, 'contact.html')
